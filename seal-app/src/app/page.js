e219482ec00137de550da41e9bc9ad356f91f025
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-09-11 19:38:22 -0700

    Remove repeating class from main

--- seal-app/src/app/page.js
+++ seal-app/src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
+    <main>
       <div className="glow relative min-h-screen">
         <div className="star top-1/3 left-1/3 size-12"></div>
         <div className="star top-1/2 right-1/2 size-12"></div>
